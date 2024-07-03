dec3219ae1cec2e51e25549916bcf7fb79547afb
Sachin Jadhav <sachin93094@gmail.com>
2024-07-03 09:46:50 +0530

    added poco lib

---
+++ Poco/build-osp/Data/CMakeFiles/Data.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/AbstractBinder.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/AbstractBinder.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/AbstractBinding.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/AbstractBinding.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/AbstractExtraction.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/AbstractExtraction.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/AbstractExtractor.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/AbstractExtractor.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/AbstractPreparation.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/AbstractPreparation.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/AbstractPreparator.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/AbstractPreparator.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/ArchiveStrategy.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/ArchiveStrategy.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Bulk.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Bulk.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Connector.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Connector.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/DataException.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/DataException.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Date.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Date.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/DynamicLOB.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/DynamicLOB.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Limit.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Limit.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/MetaColumn.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/MetaColumn.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/PooledSessionHolder.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/PooledSessionHolder.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/PooledSessionImpl.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/PooledSessionImpl.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Position.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Position.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Range.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Range.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/RecordSet.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/RecordSet.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Row.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Row.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/RowFilter.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/RowFilter.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/RowFormatter.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/RowFormatter.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/RowIterator.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/RowIterator.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/SQLChannel.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/SQLChannel.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Session.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Session.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/SessionFactory.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/SessionFactory.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/SessionImpl.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/SessionImpl.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/SessionPool.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/SessionPool.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/SessionPoolContainer.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/SessionPoolContainer.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/SimpleRowFormatter.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/SimpleRowFormatter.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Statement.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Statement.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/StatementCreator.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/StatementCreator.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/StatementImpl.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/StatementImpl.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Time.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Time.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Data/src/Transaction.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Data/CMakeFiles/Data.dir/src/Transaction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "Data_EXPORTS"
+  "POCO_ENABLE_CPP11"
+  "POCO_ENABLE_CPP14"
+  "POCO_HAVE_FD_EPOLL"
+  "POCO_OS_FAMILY_UNIX"
+  "_FILE_OFFSET_BITS=64"
+  "_LARGEFILE64_SOURCE"
+  "_REENTRANT"
+  "_THREAD_SAFE"
+  "_XOPEN_SOURCE=500"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Data/include"
+  "../Data/src"
+  "../Foundation/include"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/root/Jun_unit_Testing/2jul/Poco/build-osp/lib/libPocoData.so" "/root/Jun_unit_Testing/2jul/Poco/build-osp/lib/libPocoData.so.71"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/Jun_unit_Testing/2jul/Poco/build-osp/Foundation/CMakeFiles/Foundation.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Poco/build-osp/Util/CMakeFiles/Util.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/AbstractConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/AbstractConfiguration.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/Application.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/Application.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/ConfigurationMapper.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/ConfigurationMapper.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/ConfigurationView.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/ConfigurationView.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/FilesystemConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/FilesystemConfiguration.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/HelpFormatter.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/HelpFormatter.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/IniFileConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/IniFileConfiguration.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/IntValidator.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/IntValidator.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/JSONConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/JSONConfiguration.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/LayeredConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/LayeredConfiguration.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/LoggingConfigurator.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/LoggingConfigurator.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/LoggingSubsystem.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/LoggingSubsystem.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/MapConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/MapConfiguration.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/Option.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/Option.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/OptionCallback.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/OptionCallback.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/OptionException.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/OptionException.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/OptionProcessor.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/OptionProcessor.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/OptionSet.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/OptionSet.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/PropertyFileConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/PropertyFileConfiguration.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/RegExpValidator.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/RegExpValidator.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/ServerApplication.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/ServerApplication.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/Subsystem.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/Subsystem.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/SystemConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/SystemConfiguration.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/Timer.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/Timer.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/TimerTask.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/TimerTask.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/Validator.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/Validator.cpp.o"
+  "/root/Jun_unit_Testing/2jul/Poco/Util/src/XMLConfiguration.cpp" "/root/Jun_unit_Testing/2jul/Poco/build-osp/Util/CMakeFiles/Util.dir/src/XMLConfiguration.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "POCO_ENABLE_CPP11"
+  "POCO_ENABLE_CPP14"
+  "POCO_HAVE_FD_EPOLL"
+  "POCO_OS_FAMILY_UNIX"
+  "Util_EXPORTS"
+  "XML_DTD"
+  "_FILE_OFFSET_BITS=64"
+  "_LARGEFILE64_SOURCE"
+  "_REENTRANT"
+  "_THREAD_SAFE"
+  "_XOPEN_SOURCE=500"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Util/include"
+  "../Util/src"
+  "../Foundation/include"
+  "../XML/include"
+  "../JSON/include"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/root/Jun_unit_Testing/2jul/Poco/build-osp/lib/libPocoUtil.so" "/root/Jun_unit_Testing/2jul/Poco/build-osp/lib/libPocoUtil.so.71"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/Jun_unit_Testing/2jul/Poco/build-osp/XML/CMakeFiles/XML.dir/DependInfo.cmake"
+  "/root/Jun_unit_Testing/2jul/Poco/build-osp/JSON/CMakeFiles/JSON.dir/DependInfo.cmake"
+  "/root/Jun_unit_Testing/2jul/Poco/build-osp/Foundation/CMakeFiles/Foundation.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
